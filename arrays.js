e7dc50ee07da5439369478017fabe4e51a68d745
Mae Rine <mae.s.rine@gmail.com>
2019-07-08 17:37:08 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, ele) {
+  var newArray = [ele, ...array];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, ele) {
+  return array.unshift(ele);
+}
+
+function addElementToEndOfArray(array, ele) {
+  var newArray = [...array, ele];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, ele) {
+  return array.push(ele);
+}
+
+function accessElementInArray(array, idx) {
+  return array[idx];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var newArray = array.pop();
+  return newArray;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
